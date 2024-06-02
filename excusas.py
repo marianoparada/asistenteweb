81773d3f4b10629acbe4bad666581c4294b02fc6
Mariano <62031765+marianoparada@users.noreply.github.com>
2024-06-02 13:01:04 -0300

    Update and rename pato.py to excusas.py

--- excusas.py
+++ excusas.py
@@ -16,8 +16,8 @@ def main():
 
     st.write("Genera una excusa creativa para llegar tarde al trabajo. La excusa debe involucrar un medio de transporte y puede ser morbosa.")
 
-    modelo = st.selectbox("Selecciona el modelo de lenguaje", ["gpt-4-0613", "gpt-3.5-turbo","gpt-4o","gpt-4-turbo"])
-    mensaje = "Necesito que inventes una excusa. que hables en primera persona y te dirijas a Lucas. Tienes que poner una excusa de porque vas a llegar tarde al trabajo. Puedes inventar lo que sea, pero debes involucrar algun medio de transporte. Debes comenzar con: Hola Lucas, hoy llego un ratito mas tarde porque XXX. Debes reemplazar las XXX con la excusa. Puedes incluir algo morboso. Puedes incluir calles de Buenos Aires. Puede haber muertes de por medio. Debes hablar como argentino utilizando un lenguaje llano pero respetuoso. Debes decir a que hora llegaras aproximadamente, siempre despues de las 11."
+    modelo = st.selectbox("Selecciona el modelo de lenguaje", ["gpt-3.5-turbo","gpt-4-turbo","gpt-4o"])
+    mensaje = "Necesito que inventes una excusa. que hables en primera persona y te dirijas a Lucas. Tienes que poner una excusa de porque vas a llegar tarde al trabajo. Puedes inventar lo que sea, pero debes involucrar algun medio de transporte. Debes comenzar con: Hola Lucas, hoy llego un ratito mas tarde porque XXX. Debes reemplazar las XXX con la excusa. Puedes incluir algo morboso. Puedes incluir calles de Buenos Aires. Puede haber muertes de por medio. Debe ser en español, específicamente como si fueras argentino. Debes decir a que hora llegaras aproximadamente, siempre despues de las 11:30."
     
     if st.button("Generar Excusa"):
         respuesta = chatear(modelo, mensaje)
